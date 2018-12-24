a5f3fcb9ca39cd2c6b6ae419bd04108c8d578125
mdk44 <45779130+mdk44@users.noreply.github.com>
2018-12-24 09:19:48 -0600

    Updated Part 2, sstill WIP

--- Day 23/Day 23 Part 2 - Python.py
+++ Day 23/Day 23 Part 2 - Python.py
@@ -27,21 +27,49 @@ for line in lines:
     input_data.append(Data_Read(line))
 
 maxR = input_data[0].r
-posRx = 0
-posRy = 0
-posRz = 0
+minX = minY = minZ = 1000
+maxX = maxY = maxZ = 0
+posRx = posRy = posRz = 0
+
 for line_input in input_data:
     if line_input.r > maxR:
         maxR = line_input.r
-        posRx = line_input.x
-        posRy = line_input.y
-        posRz = line_input.z
+    if line_input.x < minX:
+        minX = line_input.x
+    if line_input.y < minY:
+        minY = line_input.y
+    if line_input.z < minZ:
+        minZ = line_input.z
+    if line_input.x > maxX:
+        maxX = line_input.x
+    if line_input.y > maxY:
+        maxY = line_input.y
+    if line_input.z > maxZ:
+        maxZ = line_input.z
 
 man_dist = 0
-range = 0
-for line_input in input_data:
-    man_dist = abs(posRx - line_input.x) + abs(posRy - line_input.y) + abs(posRz - line_input.z)
-    if man_dist <= maxR:
-        range += 1
+max_count = 0
+final_man_dist = 0
+new_man_dist = 0
+min_man_dist = 10000000
 
-print "Part 1: " + str(range)
+for x2 in range(-1000, 1000):
+    for y2 in range(-1000, 1000):
+        for z2 in range(-1000, 1000):
+            range_num =  0
+            for line_input in input_data:
+                man_dist = abs(x2 - line_input.x) + abs(y2 - line_input.y) + abs(z2 - line_input.z)
+                if man_dist <= line_input.r:
+                    range_num += 1
+            if range_num >= max_count:
+                new_man_dist = abs(x2) + abs(y2) + abs(z2)
+                if new_man_dist < min_man_dist:
+                    posRx = x2
+                    posRy = y2
+                    posRz = z2
+                max_count = range_num
+                
+
+final_man_dist = abs(posRx) + abs(posRy) + abs(posRz)
+
+print "Part 2: " + str(final_man_dist)
