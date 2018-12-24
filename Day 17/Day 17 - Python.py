7870197568706317fcb0c4a582bd04cc797bc434
mdk44 <45779130+mdk44@users.noreply.github.com>
2018-12-23 18:08:22 -0600

    Commit test

--- Day 17/Day 17 - Python.py
+++ Day 17/Day 17 - Python.py
@@ -2,7 +2,7 @@ import re
 import sys
 import time
 from PIL import Image, ImageDraw
-sys.setrecursionlimit(3000)
+sys.setrecursionlimit(3000) # Do I need this?
 
 class Clay_Bar:
     def __init__ (self, line):
