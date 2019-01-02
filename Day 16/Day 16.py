98d1a06b498dbe9e19a11d6196c867ff4cdeb5be
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-01-01 18:51:40 -0600

    More code to work

--- Day 16/Day 16.py
+++ Day 16/Day 16.py
@@ -1,1 +1,84 @@
 # Day 16
+
+import re
+
+input_file = 'Day 16\\Day 16 Test 2.txt'
+
+text_file = open(input_file)
+lines = text_file.read().split('\n')
+
+def effect(fn):
+    def final(before, instr):
+        after = list(before)
+        after[instr[3]] = fn(before, instr[1], instr[2])
+        return after
+    return final
+
+addr = effect(lambda before,x,y: before[x]+before[y])
+addi = effect(lambda before,x,y: before[x]+y)
+mulr = effect(lambda before,x,y: before[x]*before[y])
+muli = effect(lambda before,x,y: before[x]*y)
+banr = effect(lambda before,x,y: before[x] & before[y])
+bani = effect(lambda before,x,y: before[x] & y)
+borr = effect(lambda before,x,y: before[x] | before[y])
+bori = effect(lambda before,x,y: before[x] | y)
+setr = effect(lambda before,x,y: before[x])
+seti = effect(lambda before,x,y: x)
+gtir = effect(lambda before,x,y: 1 if x > before[y] else 0)
+gtri = effect(lambda before,x,y: 1 if before[x] > y else 0)
+gtrr = effect(lambda before,x,y: 1 if before[x] > before[y] else 0)
+eqir = effect(lambda before,x,y: 1 if x == before[y] else 0)
+eqri = effect(lambda before,x,y: 1 if before[x] == y else 0)
+eqrr = effect(lambda before,x,y: 1 if before[x] == before[y] else 0)
+
+opcodes = [addr, addi,
+    mulr, muli,
+    banr, bani,
+    borr, bori,
+    setr, seti,
+    gtir, gtri, gtrr,
+    eqir, eqri, eqrr]
+
+options = {}
+for code in range(16):
+    options[code] = list(enumerate(opcodes))
+
+# Part 1
+
+ans = 0
+for i in range(0, len(lines), 4):
+    if 'Before' in lines[i]:
+        before = map(int, re.findall(r'\d+', lines[i]))
+        instr = map(int, re.findall(r'\d+', lines[i+1]))
+        after = map(int, re.findall(r'\d+', lines[i+2]))
+        options[instr[0]] = [(index,fn) for (index,fn) in options[instr[0]] if fn(before,instr) == after]
+
+        matches = 0
+        for index,fn in options[instr[0]]:
+            if fn(before, instr) == after:
+                matches += 1
+        if matches >= 3:
+            ans += 1
+
+print ans
+print options[instr[0]]
+print lines
+print before
+
+# for _ in range(16):
+#     for code in range(16):
+#         if len(options[code]) == 1:
+#             for other_code in range(16):
+#                 if other_code != code:
+#                     options[other_code] = [(index,fn) for (index,fn) in options[other_code] if index!=options[code][0][0]]
+        
+
+# #for code in range(16):
+# #    print code, options[code]
+
+# registers = [0,0,0,0]
+# for line in program.strip().split('\n'):
+#     instr = map(int, re.findall('-?\d+', line))
+#     old_registers = list(registers)
+#     registers = options[instr[0]][0][1](registers, instr)
+# print registers[0]
