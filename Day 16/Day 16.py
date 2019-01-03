203c7e76f83486ac57e4831c816bf2fd0f757682
mdk44 <45779130+mdk44@users.noreply.github.com>
2019-01-03 09:30:42 -0600

    Right answer but code still needs work

--- Day 16/Day 16.py
+++ Day 16/Day 16.py
@@ -3,9 +3,12 @@
 import re
 
 input_file = 'Day 16\\Day 16 Input.txt'
+input_file2 = 'Day 16\\Day 16 Input 2.txt'
 
 text_file = open(input_file)
 lines = text_file.read().split('\n')
+text_file2 = open(input_file2)
+lines2 = text_file2.read().split('\n')
 
 def addr(before, instr):
     new_after = list(before)
@@ -106,32 +109,28 @@ def eqrr(before, instr):
     return new_after
 
 opcodes = {
-    0: addr,
-    1: addi,
-    2: mulr,
-    3: muli,
-    4: banr,
-    5: bani,
-    6: borr,
-    7: bori,
-    8: setr,
-    9: seti,
-    10: gtir,
-    11: gtri,
-    12: gtrr,
-    13: eqir,
-    14: eqri,
-    15: eqrr
+    0: addr,  # 15
+    1: addi,  # 12
+    2: mulr,  # 7
+    3: muli,  # 2
+    4: banr,  # 1
+    5: bani,  # 0
+    6: borr,  # 13
+    7: bori,  # 4
+    8: setr,  # 3
+    9: seti,  # 9
+    10: gtir,  # 6
+    11: gtri,  # 10
+    12: gtrr,  # 8
+    13: eqir,  # 14
+    14: eqri,  # 11
+    15: eqrr  # 5
 }
-
-
-options = {}
-for code in range(16):
-    options[code] = list(enumerate(opcodes))
 
 # Part 1
 
 ans = 0
+known_codes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 for i in range(0, len(lines), 4):
     if 'Before' in lines[i]:
         before = map(int, re.findall(r'\d+', lines[i]))
@@ -139,29 +138,45 @@ for i in range(0, len(lines), 4):
         after = map(int, re.findall(r'\d+', lines[i+2]))
         
         match = 0
+        code = []
         for c in opcodes:
             calc_after = opcodes[c](before, instr)
             if calc_after == after:
                 match += 1
+                if c not in known_codes:
+                    code.append(c)
         if match > 2:
             ans += 1
+        # Manually brute-forced finding each opcode's function and added the opcode index to known_codes to help find the next
+        if len(code) == 1:
+            print str(code) + ' ' + str(instr[0])
+
+registers = [0, 0, 0, 0]
+known_opcodes = {
+    15: addr,
+    12: addi,
+    7: mulr,
+    2: muli,
+    1: banr,
+    0: bani,
+    13: borr,
+    4: bori,
+    3: setr,
+    9: seti,
+    6: gtir,
+    10: gtri,
+    8: gtrr,
+    14: eqir,
+    11: eqri,
+    5: eqrr
+}
+
+c = 0
+for i in range(0, len(lines2)):
+    instr = map(int, re.findall(r'\d+', lines2[i]))
+    old_registers = list(registers)
+    c = instr[0]
+    registers = known_opcodes[c](old_registers, instr)
 
 print 'Part 1: ' + str(ans)
-
-# for _ in range(16):
-#     for code in range(16):
-#         if len(options[code]) == 1:
-#             for other_code in range(16):
-#                 if other_code != code:
-#                     options[other_code] = [(index,fn) for (index,fn) in options[other_code] if index!=options[code][0][0]]
-        
-
-# #for code in range(16):
-# #    print code, options[code]
-
-# registers = [0,0,0,0]
-# for line in program.strip().split('\n'):
-#     instr = map(int, re.findall('-?\d+', line))
-#     old_registers = list(registers)
-#     registers = options[instr[0]][0][1](registers, instr)
-# print registers[0]
+print 'Part 2: ' + str(registers[0])
